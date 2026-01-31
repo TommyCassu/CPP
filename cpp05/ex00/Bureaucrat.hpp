1a745f74e3188a2f578ec114c49cfb3471b2c55a
TommyCassu <tommy,cassu@hotmail.com>
2026-01-31 05:39:08 +0100

    cpp05 ex00

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,36 @@
+# ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+    private:
+        std::string _name;
+        int         _grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string name, int grade);
+        Bureaucrat(const Bureaucrat &bureaucrat);
+        Bureaucrat& operator=(const Bureaucrat &Bureaucrat);
+        ~Bureaucrat();
+
+        std::string getName(void) const;
+        int         getGrade(void) const;
+        void        setName(const std::string &name);
+        void        setGrade(int grade);
+        void        incrementGrade();
+        void        decrementGrade();
+
+         class      GradeTooHighException : public std::exception{
+            public:
+                virtual const char* what() const throw();
+        };
+        class      GradeTooLowException : public std::exception{
+            public:
+                virtual const char* what() const throw();
+        };
+};
+
+std::ostream    &operator<<(std::ostream &os, const Bureaucrat &employee);
+# endif
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,79 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    {
+        std::cout << "* Incrementationnal Test *" << std::endl;
+        try {
+            Bureaucrat employee("Albert", 10);
+            std::cout << employee << std::endl;
+            employee.incrementGrade();
+            std::cout << employee << std::endl;
+    
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    {
+        std::cout << std::endl;
+        std::cout << "* Decrementationnal Test *" << std::endl;
+        try {
+            Bureaucrat employee("Philipe", 10);
+            std::cout << employee << std::endl;
+            employee.decrementGrade();
+            std::cout << employee << std::endl;
+    
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    {
+        std::cout << std::endl;
+        std::cout << "* Decrementationnal under limite Test *" << std::endl;
+        try {
+            Bureaucrat employee("Jason", 150);
+            std::cout << employee << std::endl;
+            employee.decrementGrade();
+            std::cout << employee << std::endl;
+    
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    {
+        std::cout << std::endl;
+        std::cout << "* Decrementationnal above limite Test *" << std::endl;
+        try {
+            Bureaucrat employee("Richie", 1);
+            std::cout << employee << std::endl;
+            employee.incrementGrade();
+            std::cout << employee << std::endl;
+    
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    {
+        std::cout << std::endl;
+        std::cout << "* Initialization under limite Test *" << std::endl;
+        try {
+            Bureaucrat employee("Marc", 151);
+            std::cout << employee << std::endl;
+    
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    {
+        std::cout << std::endl;
+        std::cout << "* Initialization above limite Test *" << std::endl;
+        try {
+            Bureaucrat employee("Pedro", 0);
+            std::cout << employee << std::endl;
+    
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    return (0);
+}
