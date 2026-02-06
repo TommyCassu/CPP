8a82c1295278a1910a4ee1d234b7ff2ac9bb28d0
TommyCassu <tommy.cassu@hotmail.com>
2026-02-06 16:46:28 +0100

    cpp06 ex00 add specialInoput and continue to improve  code ( close to finish)

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <sstream>
 #include <limits.h>
+# include <cfloat>
+#include <cmath>
 
 ScalarConverter::ScalarConverter() {
     std::cout << "ScalarConverter default constructor called" << std::endl;
@@ -49,8 +51,9 @@ int getInt(std::string str) {
 double getDouble(std::string str) {
     std::istringstream stream(str);
     double returnedDouble;
-    if (stream >> returnedDouble)
+    if (stream >> returnedDouble && (stream.peek() == -1 || stream.peek() == 'f'))
         return returnedDouble;
+    throw ScalarConverter::InvalidInputException();
     return 0;
 }
 
@@ -63,28 +66,94 @@ float getFloat(std::string str) {
 }
 
 void    printChar(std::string value) {
-    if (value.length() == 1) {
-        if ((!isdigit(static_cast<unsigned char>(value[0]))) && isprint(static_cast<unsigned char>(value[0])))
-            std::cout << static_cast<char>(value[0]) << std::endl;
+    //gestion int 0-9
+    if (value.length() == 1 && !isdigit(static_cast<unsigned char>(value[0]))) {
+        if (isprint(static_cast<unsigned char>(value[0])))
+            std::cout << "char: " << static_cast<char>(value[0]) << std::endl;
         if (!isprint(static_cast<unsigned char>(value[0])))
             throw ScalarConverter::InvalidInputNoPrintableException();
     }
     else {
-        double returnedInt = getDouble(value);
-        if (returnedInt > INT_MAX || returnedInt < INT_MIN)
+        double returnedDouble = getDouble(value);
+        if (returnedDouble < 32 || returnedDouble > 126)
             std::cout << "char: Non displayable" << std::endl;
-        else if (isprint(static_cast<unsigned char>(value[0])))
-            std::cout << static_cast<char>(value[0]) << std::endl;
+        else
+            std::cout << "char: " << static_cast<char>(returnedDouble) << std::endl;
     }
 }
 
 void    printInt(std::string value) {
-    double returnedInt = getDouble(value);
-        std::cout << returnedInt << std::endl;
-    if (returnedInt < INT_MIN || INT_MAX)
-        std::cout << "int: impossible (out of limits)" << std::endl;
-    else
-        std::cout << "int: " << static_cast<int>(returnedInt) << std::endl;
+    double returnedDouble = getDouble(value);
+    if (value.length() == 1) {
+        if (!isdigit(value[0]))
+            std::cout << "int: " << static_cast<int>(value[0]) << std::endl;
+        else
+            std::cout << "int: " << static_cast<int>(returnedDouble) << std::endl;
+    }
+    else {
+        
+        if (returnedDouble < INT_MIN || returnedDouble > INT_MAX)
+            std::cout << "int: impossible (out of limits)" << std::endl;
+        else
+            std::cout << "int: " << static_cast<int>(returnedDouble) << std::endl;
+    }
+}
+
+void    printFloat(std::string value) {
+    double returnedDouble = getDouble(value);
+    if (value.length() == 1) {
+        if (!isdigit(value[0]))
+            std::cout << "float: " << static_cast<float>(value[0]) << ".0f" << std::endl;
+        else
+            std::cout << "float: " << static_cast<float>(returnedDouble) << ".0f" << std::endl;
+    }
+    else {
+        if (returnedDouble < -FLT_MAX || returnedDouble > FLT_MAX)
+            std::cout << "float: impossible (out of limits)" << std::endl;
+        else
+            std::cout << "float: " << static_cast<float>(returnedDouble) << "f" << std::endl;
+    }
+}
+
+void    printDouble(std::string value) {
+    double returnedDouble = getDouble(value);
+    if (value.length() == 1) {
+        if (!isdigit(value[0]))
+            std::cout << "double: " << static_cast<double>(value[0]) << ".0" << std::endl;
+        else
+            std::cout << "double: " << static_cast<double>(returnedDouble) << ".0" << std::endl;
+    }
+    else {
+        if (returnedDouble < -DBL_MAX || returnedDouble > DBL_MAX)
+            std::cout << "double: impossible (out of limits)" << std::endl;
+        else
+            std::cout << "double: " << static_cast<double>(returnedDouble) << std::endl;
+    }
+}
+
+void    printFunny(std::string value) {
+    if (value == "nan" || value == "nanf") {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: nanf" << std::endl;
+        std::cout << "double: nan" << std::endl;
+    }
+    else {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        if (value.find('-')) {
+            std::cout << "float: -inff" << std::endl;
+            std::cout << "double: -inf" << std::endl;
+        }
+        else {
+            std::cout << "float: +inff" << std::endl;
+            std::cout << "double: +inf" << std::endl;
+        }
+    }
+}
+
+bool    isFunnyValue(std::string value) {
+    return (value == "nan" || value == "nanf" || value == "-inf" || value == "+inf" || value == "-inff" || value == "+inff");
 }
 
 TypeFounded parsingString(std::string value) {
@@ -95,56 +164,32 @@ TypeFounded parsingString(std::string value) {
         if (!isprint(static_cast<unsigned char>(value[0])))
             throw ScalarConverter::InvalidInputNoPrintableException();
     }
-    if (getInt(value)) {
-        return INT;
-    }
+    
     int nb_f = countOccurence(value, 'f');
     int nb_dot = countOccurence(value, '.');
     if ((nb_f == 1 && nb_dot == 1 && getFloat(value))) {
         return FLOAT;
     }
-    if ((nb_f == 0 && nb_dot == 1 && getDouble(value))  || getInt(value)){
+    if ((nb_f == 0 && nb_dot == 1 && getDouble(value))){
         return DOUBLE;
     }
-    
-    return INT;
+    if (getDouble(value)) {
+        return INT;
+    }
+    return DOUBLE;
 };
 
 void    ScalarConverter::convert(std::string const &value) {
+    
+    double doubleResult;
+    if (isFunnyValue(value))
+        return printFunny(value);
     try {
-        TypeFounded type = parsingString(value);
-        float floatResult;
-        double doubleResult;
-        switch (type) {
-            case CHAR:
-                std::cout << "char: " << value[0] << std::endl;
-                std::cout << "int: " << static_cast<int>(value[0]) << std::endl;
-                std::cout << "float: " << static_cast<float>(value[0]) << ".0f" << std::endl;
-                std::cout << "double: " << static_cast<double>(value[0]) << ".0" << std::endl;
-                break;
-            case INT: {
-                printChar(value);
-                printInt(value);
-                //std::cout << "float: " << static_cast<float>(returnedInt) << ".0f" << std::endl;
-                //std::cout << "double: " << static_cast<double>(returnedInt) << ".0" << std::endl;
-                break;
-            }
-            case FLOAT:
-                std::cout << "char : Non displayable" << std::endl;
-                std::cout << "int: " << getInt(value) << std::endl;
-                std::cout << "float: " << getFloat(value) << "f" << std::endl;
-                std::cout << "double: " << getDouble(value) << std::endl;
-                break;
-            case DOUBLE:
-                std::cout << "char : Non displayable" << std::endl;
-                std::cout << "int: " << getInt(value) << std::endl;
-                std::cout << "float: " << getFloat(value) << "f" << std::endl;
-                std::cout << "double: " << getDouble(value) << std::endl;
-                break;
-            case UNKNOW:
-                throw InvalidInputException();
-        }
-
+        doubleResult = getDouble(value);
+        printChar(value);
+        printInt(value);
+        printFloat(value);
+        printDouble(value);
     } catch (const std::exception &e) {
         std::cout << "Error ! " << e.what() << std::endl;
     }  
