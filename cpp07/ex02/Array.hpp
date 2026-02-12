70aab1868d7891161e2f6947ac1ae4e4fd2f2ddf
TommyCassu <tommy.cassu@hotmail.com>
2026-02-12 04:54:16 +0100

    cpp07/ex02 done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -12,7 +12,7 @@ class Array {
         Array() : _elements(new T[0]), _size(0) {
             std::cout << "Array template default constructor called" << std::endl;
         };
-        Array(unsigned int n) : _elements(new T[n]), _size(0) {
+        Array(unsigned int n) : _elements(new T[n]), _size(n) {
             std::cout << "Array template param(s) constructor called" << std::endl;
         };
         Array(const Array &oldArray) : _elements(new T[oldArray._size]), _size(oldArray._size) {
@@ -20,6 +20,7 @@ class Array {
                 _elements[i] = oldArray._elements[i];
             std::cout << "Array template Copy constructor called" << std::endl;
         };
+        ~Array() { delete[] _elements;}
         Array& operator=(const Array &oldArray) {
             if (this != &oldArray) {
                 delete _elements;
@@ -31,6 +32,22 @@ class Array {
             std::cout << "Array template assignment operator called" << std::endl;
             return *this;
         };
+        
+        class      OutOfRangeException : public std::exception{
+            public:
+                virtual const char* what() const throw() {
+                    return ("Size input is out of range !");
+                };
+        };
+
+        T& operator[](unsigned int n){
+            if (n >= _size)
+                throw OutOfRangeException();
+            return _elements[n];
+        };
+
+        unsigned int size() { return _size; };
+
 };
 
 # endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -2,6 +2,53 @@
 #include <iostream>
 
 int main( void ) {
-    int * a = new int();
-    std::cout << *a << std::endl;
+    {
+        try {
+            Array<int> a(5);
+            for (unsigned int i = 0; i < a.size(); i++) {
+                a[i] = i;
+            };
+            std::cout << "Array print :" << std::endl;
+            for (unsigned int i = 0; i < a.size(); i++) {
+                std::cout << a[i] << " ";
+            };
+            std::cout << std::endl;
+        } catch (const std::exception& e) {
+            std::cout << "Error ! " << e.what() << std::endl;
+        }
+    }
+    std::cout << std::endl;
+    {
+        try {
+            Array<double> a(5);
+            for (unsigned int i = 0; i < a.size(); i++) {
+                a[i] = static_cast<double>(i) + 0.5;
+            };
+            std::cout << "Array print :" << std::endl;
+            for (unsigned int i = 0; i < a.size(); i++) {
+                std::cout << a[i] << " ";
+            };
+            std::cout << std::endl;
+        } catch (const std::exception& e) {
+            std::cout << "Error ! " << e.what() << std::endl;
+        }
+    }
+    std::cout << std::endl;
+    {
+        try {
+            Array<int> a(10);
+            for (unsigned int i = 0; i < 11; i++) {
+                a[i] = i;
+            };
+            std::cout << "Array print :" << std::endl;
+            for (unsigned int i = 0; i < a.size(); i++) {
+                std::cout << a[i] << " ";
+            };
+            std::cout << std::endl;
+        } catch (const std::exception& e) {
+            std::cout << "Error ! " << e.what() << std::endl;
+        }
+        
+    }
+
 }
