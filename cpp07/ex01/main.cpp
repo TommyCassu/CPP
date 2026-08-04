ce52bebaf2d8744edae095963f930f29a98d3a82
TommyCassu <tommy.cassu@hotmail.com>
2026-08-04 22:27:46 +0200

    fix cpp07

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,5 +1,5 @@
 #include "iter.hpp"
-#include <iostream>
+
 
 
 int main( void ) {
--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -9,15 +9,15 @@ class Array {
         T* _elements;
         unsigned int _size;
     public:
-        Array() : _elements(new T[0]), _size(0) {
+        Array() : _elements(new T[0]()), _size(0) {
             std::cout << "Array template default constructor called" << std::endl;
         };
 
-        Array(unsigned int n) : _elements(new T[n]), _size(n) {
+        Array(unsigned int n) : _elements(new T[n]()), _size(n) {
             std::cout << "Array template param(s) constructor called" << std::endl;
         };
 
-        Array(const Array &oldArray) : _elements(new T[oldArray._size]), _size(oldArray._size) {
+        Array(const Array &oldArray) : _elements(new T[oldArray._size]()), _size(oldArray._size) {
             for (unsigned int i = 0; i < _size; i++)
                 _elements[i] = oldArray._elements[i];
             std::cout << "Array template Copy constructor called" << std::endl;
